a1ed52ffc99fbd7332917b78cf627022e183df85
deriifirgiawan <derifirgiawan025@gmail.com>
2024-10-06 06:46:24 +0700

    feat: add use client

--- src/app/dashboard/data-politik/page.tsx
+++ src/app/dashboard/data-politik/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Dashboard, DataPolitik, MainLayout } from "@/core";
 
 export default function DataPolitikPage() {
--- src/app/dashboard/rekomendasi-program/page.tsx
+++ src/app/dashboard/rekomendasi-program/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { MainLayout } from "@/core";
 
 export default function RekomendasiProgramPage() {
